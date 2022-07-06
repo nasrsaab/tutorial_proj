3789eaf3b54dd369df45575c9e185b05ce26b273
NasrEldin Saab-2022 <98010047+nasrsaab@users.noreply.github.com>
2022-07-06 12:16:53 +0300

    this is a module file to the main file

---
+++ module.py
@@ -0,0 +1,21 @@
+import commit
+# importing pandas module...to create a data frame
+
+import pandas as pd
+
+'''
+module for git commit file
+'''
+
+
+url = input("Please enter your URL: ")
+commit.show_commit(url)
+# https://github.com/nasrsaab/Our_Project.git
+# https://github.com/nasrsaab/Git_course.git
+
+
+'''open the xlsx file and put the data in dataframe '''
+df_dataframe = pd.read_excel('Excel_File1.xlsx')
+
+# print the dataframe
+print(df_dataframe)
